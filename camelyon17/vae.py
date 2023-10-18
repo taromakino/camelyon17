0a4df13b01fe0316b2769977118f5db8a4a8c886
makinot1 <makinot1@gene.com>
2023-10-19 08:51:13 +0900

    Bug fix, had zs instead of zc

--- camelyon17/vae.py
+++ camelyon17/vae.py
@@ -310,7 +310,7 @@ class VAE(pl.LightningModule):
         log_prob_y_zc = -F.binary_cross_entropy_with_logits(y_pred, y.float(), reduction='none')
         # log q(z_c,z_s|x,y,e)
         joint, causal, spurious = self.encoder(x, y, e)
-        log_prob_zc = causal.log_prob(z_s)
+        log_prob_zc = causal.log_prob(z_c)
         loss = -log_prob_x_z - self.y_mult * log_prob_y_zc - self.alpha * log_prob_zc
         return loss
 
