584596e9947a97c25537d52431ec6b644c73f05b
makinot1 <makinot1@gene.com>
2023-10-19 23:15:35 +0900

    Revert to setup that gets 79% test acc

--- camelyon17/vae.py
+++ camelyon17/vae.py
@@ -7,11 +7,11 @@ from data import N_CLASSES, N_ENVS
 from torch.optim import Adam
 from torchmetrics import Accuracy
 from utils.enums import Task
-from utils.nn_utils import MLP, arr_to_cov, arr_to_tril
+from utils.nn_utils import MLP, arr_to_cov
 
 
 CNN_SIZE = 864
-PRIOR_INIT_SD = 0.1
+PRIOR_INIT_SD = 0.01
 
 
 class DenseLayer(nn.Module):
@@ -165,7 +165,6 @@ class Encoder(nn.Module):
         diag_causal = diag_causal.reshape(batch_size, N_ENVS, self.z_size)
         diag_causal = diag_causal[torch.arange(batch_size), e, :]
         cov_causal = arr_to_cov(low_rank_causal, diag_causal)
-        causal_dist = D.MultivariateNormal(mu_causal, scale_tril=torch.linalg.cholesky(cov_causal))
         # Spurious
         mu_spurious = self.mu_spurious(x)
         mu_spurious = mu_spurious.reshape(batch_size, N_CLASSES, N_ENVS, self.z_size)
@@ -177,14 +176,12 @@ class Encoder(nn.Module):
         diag_spurious = diag_spurious.reshape(batch_size, N_CLASSES, N_ENVS, self.z_size)
         diag_spurious = diag_spurious[torch.arange(batch_size), y, e, :]
         cov_spurious = arr_to_cov(low_rank_spurious, diag_spurious)
-        spurious_dist = D.MultivariateNormal(mu_spurious, scale_tril=torch.linalg.cholesky(cov_spurious))
         # Block diagonal
         mu = torch.hstack((mu_causal, mu_spurious))
         cov = torch.zeros(batch_size, 2 * self.z_size, 2 * self.z_size, device=y.device)
         cov[:, :self.z_size, :self.z_size] = cov_causal
         cov[:, self.z_size:, self.z_size:] = cov_spurious
-        joint_dist = D.MultivariateNormal(mu, scale_tril=torch.linalg.cholesky(cov))
-        return joint_dist, causal_dist, spurious_dist
+        return D.MultivariateNormal(mu, scale_tril=torch.linalg.cholesky(cov))
 
 
 class Decoder(nn.Module):
@@ -266,7 +263,7 @@ class VAE(pl.LightningModule):
 
     def loss(self, x, y, e):
         # z_c,z_s ~ q(z_c,z_s|x)
-        posterior_dist, _, _ = self.encoder(x, y, e)
+        posterior_dist = self.encoder(x, y, e)
         z = self.sample_z(posterior_dist)
         # E_q(z_c,z_s|x)[log p(x|z_c,z_s)]
         log_prob_x_z = self.decoder(x, z).mean()
@@ -309,18 +306,15 @@ class VAE(pl.LightningModule):
         y_pred = self.classifier(z_c).view(-1)
         log_prob_y_zc = -F.binary_cross_entropy_with_logits(y_pred, y.float(), reduction='none')
         # log q(z_c,z_s|x,y,e)
-        _, causal_dist, spurious_dist = self.encoder(x, y, e)
-        log_prob_zc = causal_dist.log_prob(z_c)
-        log_prob_zs = spurious_dist.log_prob(z_s)
-        loss = -log_prob_x_z - self.y_mult * log_prob_y_zc - log_prob_zc - self.alpha * log_prob_zs
+        log_prob_z_xye = self.encoder(x, y, e).log_prob(z)
+        loss = -log_prob_x_z - self.y_mult * log_prob_y_zc - self.alpha * log_prob_z_xye
         return loss
 
     def make_z_param(self, x, y_value, e_value):
         batch_size = len(x)
         y = torch.full((batch_size,), y_value, dtype=torch.long, device=self.device)
         e = torch.full((batch_size,), e_value, dtype=torch.long, device=self.device)
-        joint, _, _ = self.encoder(x, y, e)
-        return nn.Parameter(joint.loc.detach())
+        return nn.Parameter(self.encoder(x, y, e).loc.detach())
 
     def opt_infer_loss(self, x, y_value, e_value):
         batch_size = len(x)
