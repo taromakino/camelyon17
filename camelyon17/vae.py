9541361c4d42cecd9addce257b87b45879d184ad
makinot1 <makinot1@gene.com>
2023-10-19 09:25:55 +0900

    Apply alpha to log q(z|x,y,e)

--- camelyon17/vae.py
+++ camelyon17/vae.py
@@ -309,10 +309,9 @@ class VAE(pl.LightningModule):
         y_pred = self.classifier(z_c).view(-1)
         log_prob_y_zc = -F.binary_cross_entropy_with_logits(y_pred, y.float(), reduction='none')
         # log q(z_c,z_s|x,y,e)
-        _, causal, spurious = self.encoder(x, y, e)
-        log_prob_zc = causal.log_prob(z_c)
-        log_prob_zs = spurious.log_prob(z_s)
-        loss = -log_prob_x_z - self.y_mult * log_prob_y_zc - log_prob_zc - self.alpha * log_prob_zs
+        joint, _, _ = self.encoder(x, y, e)
+        log_prob_posterior = joint.log_prob(z)
+        loss = -log_prob_x_z - self.y_mult * log_prob_y_zc - self.alpha * log_prob_posterior
         return loss
 
     def make_z_param(self, x, y_value, e_value):
