a8c2298140a09583f58c7e028111d6f08552d2db
makinot1 <makinot1@gene.com>
2023-11-01 21:31:02 +0900

    BN in MLP

--- camelyon17/utils/nn_utils.py
+++ camelyon17/utils/nn_utils.py
@@ -15,6 +15,7 @@ class MLP(nn.Module):
         for hidden_dim in x_sizes:
             module_list.append(nn.Linear(last_in_dim, hidden_dim))
             module_list.append(nn.LeakyReLU())
+            module_list.append(nn.BatchNorm1d())
             last_in_dim = hidden_dim
         module_list.append(nn.Linear(last_in_dim, output_size))
         self.module_list = nn.Sequential(*module_list)
