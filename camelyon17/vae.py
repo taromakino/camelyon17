265f18bf16b9a82238fa967e3722fe5ca612078f
makinot1 <makinot1@gene.com>
2023-11-05 13:32:00 -0500

    Simple CNN/DCNN

--- camelyon17/vae.py
+++ camelyon17/vae.py
@@ -1,261 +1,72 @@
+import numpy as np
 import pytorch_lightning as pl
 import torch
 import torch.distributions as D
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.utils.checkpoint as cp
-from collections import OrderedDict
 from data import N_CLASSES, N_ENVS
-from torch import Tensor
 from torch.optim import Adam
 from torchmetrics import Accuracy
-from typing import List, Tuple
 from utils.enums import Task
 from utils.nn_utils import MLP, arr_to_cov, one_hot
 
 
-CNN_SIZE = 96 * 3 * 3
-
-
-class _DenseLayer(nn.Module):
-    def __init__(
-        self, num_input_features: int, growth_rate: int, bn_size: int, drop_rate: float, memory_efficient: bool = False,
-        is_encoder: bool = True
-    ) -> None:
-        super().__init__()
-        self.norm1 = nn.BatchNorm2d(num_input_features)
-        self.relu1 = nn.ReLU(inplace=True)
-        if is_encoder:
-            self.conv1 = nn.Conv2d(num_input_features, bn_size * growth_rate, kernel_size=1, stride=1, bias=False)
-            self.conv2 = nn.Conv2d(bn_size * growth_rate, growth_rate, kernel_size=3, stride=1, padding=1, bias=False)
-        else:
-            self.conv1 = nn.ConvTranspose2d(num_input_features, bn_size * growth_rate, kernel_size=1, stride=1, bias=False)
-            self.conv2 = nn.ConvTranspose2d(bn_size * growth_rate, growth_rate, kernel_size=3, stride=1, padding=1, bias=False)
-        self.norm2 = nn.BatchNorm2d(bn_size * growth_rate)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.drop_rate = float(drop_rate)
-        self.memory_efficient = memory_efficient
-
-    def bn_function(self, inputs: List[Tensor]) -> Tensor:
-        concated_features = torch.cat(inputs, 1)
-        bottleneck_output = self.conv1(self.relu1(self.norm1(concated_features)))  # noqa: T484
-        return bottleneck_output
-
-    # todo: rewrite when torchscript supports any
-    def any_requires_grad(self, input: List[Tensor]) -> bool:
-        for tensor in input:
-            if tensor.requires_grad:
-                return True
-        return False
-
-    @torch.jit.unused  # noqa: T484
-    def call_checkpoint_bottleneck(self, input: List[Tensor]) -> Tensor:
-        def closure(*inputs):
-            return self.bn_function(inputs)
-
-        return cp.checkpoint(closure, *input)
-
-    @torch.jit._overload_method  # noqa: F811
-    def forward(self, input: List[Tensor]) -> Tensor:  # noqa: F811
-        pass
-
-    @torch.jit._overload_method  # noqa: F811
-    def forward(self, input: Tensor) -> Tensor:  # noqa: F811
-        pass
-
-    # torchscript does not yet support *args, so we overload method
-    # allowing it to take either a List[Tensor] or single Tensor
-    def forward(self, input: Tensor) -> Tensor:  # noqa: F811
-        if isinstance(input, Tensor):
-            prev_features = [input]
-        else:
-            prev_features = input
-
-        if self.memory_efficient and self.any_requires_grad(prev_features):
-            if torch.jit.is_scripting():
-                raise Exception("Memory Efficient not supported in JIT")
-
-            bottleneck_output = self.call_checkpoint_bottleneck(prev_features)
-        else:
-            bottleneck_output = self.bn_function(prev_features)
-
-        new_features = self.conv2(self.relu2(self.norm2(bottleneck_output)))
-        if self.drop_rate > 0:
-            new_features = F.dropout(new_features, p=self.drop_rate, training=self.training)
-        return new_features
-
-
-class _DenseBlock(nn.ModuleDict):
-    _version = 2
-
-    def __init__(
-        self,
-        num_layers: int,
-        num_input_features: int,
-        bn_size: int,
-        growth_rate: int,
-        drop_rate: float,
-        memory_efficient: bool = False,
-        is_encoder: bool = True
-    ) -> None:
-        super().__init__()
-        for i in range(num_layers):
-            layer = _DenseLayer(
-                num_input_features + i * growth_rate,
-                growth_rate=growth_rate,
-                bn_size=bn_size,
-                drop_rate=drop_rate,
-                memory_efficient=memory_efficient,
-                is_encoder=is_encoder
-            )
-            self.add_module("denselayer%d" % (i + 1), layer)
-
-    def forward(self, init_features: Tensor) -> Tensor:
-        features = [init_features]
-        for name, layer in self.items():
-            new_features = layer(features)
-            features.append(new_features)
-        return torch.cat(features, 1)
-
-
-class _Transition(nn.Sequential):
-    def __init__(self, num_input_features: int, num_output_features: int, is_encoder: bool = True) -> None:
-        super().__init__()
-        self.norm = nn.BatchNorm2d(num_input_features)
-        self.relu = nn.ReLU(inplace=True)
-        if is_encoder:
-            self.conv = nn.Conv2d(num_input_features, num_output_features, kernel_size=1, stride=1, bias=False)
-            self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
-        else:
-            self.conv = nn.ConvTranspose2d(num_input_features, num_output_features, kernel_size=1, stride=1, bias=False)
-            self.pool = nn.ConvTranspose2d(num_output_features, num_output_features, 2, 2)
+CNN_SHAPE = (512, 3, 3)
+CNN_SIZE = np.prod(CNN_SHAPE)
 
 
 class CNN(nn.Module):
-    def __init__(
-        self,
-        growth_rate: int = 32,
-        block_config: Tuple[int, int, int, int] = (3, 3, 3, 3),
-        num_init_features: int = 96,
-        bn_size: int = 4,
-        drop_rate: float = 0,
-        memory_efficient: bool = False
-    ) -> None:
-
+    def __init__(self):
         super().__init__()
-
-        # First convolution
-        self.features = nn.Sequential(
-            OrderedDict(
-                [
-                    ("conv0", nn.Conv2d(3, num_init_features, kernel_size=3, stride=2, padding=1, bias=False)),
-                    ("norm0", nn.BatchNorm2d(num_init_features)),
-                    ("relu0", nn.ReLU(inplace=True))
-                ]
+        modules = []
+        hidden_dims = [32, 64, 128, 256, 512]
+        in_channels = 3
+        for h_dim in hidden_dims:
+            modules.append(
+                nn.Sequential(
+                    nn.Conv2d(in_channels, out_channels=h_dim, kernel_size=3, stride=2, padding=1),
+                    nn.BatchNorm2d(h_dim),
+                    nn.LeakyReLU())
             )
-        )
+            in_channels = h_dim
+        self.sequential = nn.Sequential(*modules)
 
-        # Each denseblock
-        num_features = num_init_features
-        for i, num_layers in enumerate(block_config):
-            block = _DenseBlock(
-                num_layers=num_layers,
-                num_input_features=num_features,
-                bn_size=bn_size,
-                growth_rate=growth_rate,
-                drop_rate=drop_rate,
-                memory_efficient=memory_efficient,
-            )
-            self.features.add_module("denseblock%d" % (i + 1), block)
-            num_features = num_features + num_layers * growth_rate
-            if i != len(block_config) - 1:
-                trans = _Transition(num_input_features=num_features, num_output_features=num_features // 2)
-                self.features.add_module("transition%d" % (i + 1), trans)
-                num_features = num_features // 2
-
-        # Final batch norm
-        self.features.add_module("norm5", nn.BatchNorm2d(num_features))
-        self.features.add_module("relu5", nn.ReLU(inplace=True))
-        self.features.add_module("down_conv", nn.Conv2d(num_features, num_init_features, 2, 2))
-
-        # Official init from torch repo.
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.constant_(m.bias, 0)
-
-    def forward(self, x: Tensor) -> Tensor:
-        features = self.features(x)
-        out = F.relu(features, inplace=True)
-        return out
+    def forward(self, x):
+        return self.sequential(x)
 
 
 class DCNN(nn.Module):
-    def __init__(
-        self,
-        growth_rate: int = 32,
-        block_config: Tuple[int, int, int, int] = (3, 3, 3, 3),
-        num_init_features: int = 96,
-        bn_size: int = 4,
-        drop_rate: float = 0,
-        memory_efficient: bool = False
-    ) -> None:
-
+    def __init__(self):
         super().__init__()
-
-        # First convolution
-        self.features = nn.Sequential(
-            OrderedDict(
-                [
-                    ("conv0", nn.ConvTranspose2d(num_init_features, num_init_features, 2, 2)),
-                    ("norm0", nn.BatchNorm2d(num_init_features)),
-                    ("relu0", nn.ReLU(inplace=True))
-                ]
+        self.hidden_dims = [512, 256, 128, 64, 32]
+        modules = []
+        for i in range(len(self.hidden_dims) - 1):
+            modules.append(
+                nn.Sequential(
+                    nn.ConvTranspose2d(
+                        self.hidden_dims[i],
+                        self.hidden_dims[i + 1],
+                        kernel_size=3,
+                        stride=2,
+                        padding=1,
+                        output_padding=1),
+                    nn.BatchNorm2d(self.hidden_dims[i + 1]),
+                    nn.LeakyReLU())
             )
-        )
+        modules.append(nn.ConvTranspose2d(
+            self.hidden_dims[-1],
+            self.hidden_dims[-1],
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            output_padding=1))
+        modules.append(nn.BatchNorm2d(self.hidden_dims[-1]))
+        modules.append(nn.LeakyReLU())
+        modules.append(nn.Conv2d(self.hidden_dims[-1], out_channels=3, kernel_size=3, padding=1))
+        self.sequential = nn.Sequential(*modules)
 
-        # Each denseblock
-        num_features = num_init_features
-        for i, num_layers in enumerate(block_config):
-            block = _DenseBlock(
-                num_layers=num_layers,
-                num_input_features=num_features,
-                bn_size=bn_size,
-                growth_rate=growth_rate,
-                drop_rate=drop_rate,
-                memory_efficient=memory_efficient,
-                is_encoder=False
-            )
-            self.features.add_module("denseblock%d" % (i + 1), block)
-            num_features = num_features + num_layers * growth_rate
-            if i != len(block_config) - 1:
-                trans = _Transition(num_input_features=num_features, num_output_features=num_features // 2, is_encoder=False)
-                self.features.add_module("transition%d" % (i + 1), trans)
-                num_features = num_features // 2
-
-        # Final batch norm
-        self.features.add_module("norm5", nn.BatchNorm2d(num_features))
-        self.features.add_module("relu5", nn.ReLU(inplace=True))
-        self.features.add_module("down_conv", nn.ConvTranspose2d(num_features, 3, 3, 2, 1, 1))
-
-        # Official init from torch repo.
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.constant_(m.bias, 0)
-
-    def forward(self, x: Tensor) -> Tensor:
-        features = self.features(x)
-        out = F.relu(features, inplace=True)
-        return out
+    def forward(self, x):
+        return self.sequential(x)
 
 
 class Encoder(nn.Module):
@@ -304,7 +115,7 @@ class Decoder(nn.Module):
 
     def forward(self, x, z):
         batch_size = len(x)
-        x_pred = self.mlp(z).view(batch_size, 96, 3, 3)
+        x_pred = self.mlp(z).view(batch_size, *CNN_SHAPE)
         x_pred = self.dcnn(x_pred).view(batch_size, -1)
         return -F.binary_cross_entropy_with_logits(x_pred, x.view(batch_size, -1), reduction='none').sum(dim=1)
 
