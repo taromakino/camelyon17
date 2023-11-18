3b175a2f10b61447bee38bae882bf8e4fae6c093
makinot1 <makinot1@gene.com>
2023-11-18 15:55:28 -0500

    BatchNorm in MLP

--- camelyon17/utils/nn_utils.py
+++ camelyon17/utils/nn_utils.py
@@ -8,15 +8,16 @@ COV_OFFSET = 1e-6
 
 
 class MLP(nn.Module):
-    def __init__(self, input_size, x_sizes, output_size):
+    def __init__(self, input_size, h_sizes, output_size):
         super().__init__()
         module_list = []
-        last_in_dim = input_size
-        for hidden_dim in x_sizes:
-            module_list.append(nn.Linear(last_in_dim, hidden_dim))
-            module_list.append(nn.LeakyReLU())
-            last_in_dim = hidden_dim
-        module_list.append(nn.Linear(last_in_dim, output_size))
+        last_size = input_size
+        for h_size in h_sizes:
+            module_list.append(nn.Linear(last_size, h_size))
+            module_list.append(nn.ReLU())
+            module_list.append(nn.BatchNorm2d(h_size))
+            last_size = h_size
+        module_list.append(nn.Linear(last_size, output_size))
         self.module_list = nn.Sequential(*module_list)
 
     def forward(self, *args):
