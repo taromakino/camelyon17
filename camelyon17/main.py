bb55faa6fb6908ba1ff66a2391274704703d0957
makinot1 <makinot1@gene.com>
2023-11-28 11:01:25 -0500

    Minor changes

--- camelyon17/main.py
+++ camelyon17/main.py
@@ -93,7 +93,7 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--classify_batch_size', type=int, default=1024)
     parser.add_argument('--n_workers', type=int, default=8)
-    parser.add_argument('--n_subsample', type=int, default=1024)
+    parser.add_argument('--n_subsample', type=int, default=None)
     parser.add_argument('--z_size', type=int, default=128)
     parser.add_argument('--rank', type=int, default=64)
     parser.add_argument('--h_sizes', nargs='+', type=int, default=[512, 512])
