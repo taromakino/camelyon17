405b2a9b34bdde77fb1b78920331de1426687e21
makinot1 <makinot1@gene.com>
2023-12-05 23:19:29 -0500

    Smaller EncoderCNN

--- camelyon17/encoder_cnn.py
+++ camelyon17/encoder_cnn.py
@@ -1,13 +1,50 @@
 import torch.nn as nn
-from torchvision.models import densenet121
+from collections import OrderedDict
+from torch import Tensor
+from typing import Tuple
+from torchvision.models.densenet import _DenseBlock, _Transition
 
 
 class EncoderCNN(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.densenet = densenet121()
-        del self.densenet.classifier
-        self.densenet.classifier = nn.Identity()
+    def __init__(
+        self,
+        growth_rate: int = 8,
+        block_config: Tuple[int, int, int, int] = (3, 3, 3, 3, 3),
+        num_init_features: int = 24,
+        bn_size: int = 4,
+        drop_rate: float = 0,
+        memory_efficient: bool = False,
+    ) -> None:
 
-    def forward(self, x):
-        return self.densenet(x)
+        super().__init__()
+
+        self.features = nn.Sequential(
+            OrderedDict(
+                [
+                    ("conv0", nn.Conv2d(3, num_init_features, kernel_size=3, padding=1, bias=False)),
+                    ("norm0", nn.BatchNorm2d(num_init_features)),
+                    ("relu0", nn.ReLU(inplace=True))
+                ]
+            )
+        )
+
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            block = _DenseBlock(
+                num_layers=num_layers,
+                num_input_features=num_features,
+                bn_size=bn_size,
+                growth_rate=growth_rate,
+                drop_rate=drop_rate,
+                memory_efficient=memory_efficient,
+            )
+            self.features.add_module("denseblock%d" % (i + 1), block)
+            num_features = num_features + num_layers * growth_rate
+            if i != len(block_config) - 1:
+                trans = _Transition(num_input_features=num_features, num_output_features=num_features // 2)
+                self.features.add_module("transition%d" % (i + 1), trans)
+                num_features = num_features // 2
+
+    def forward(self, x: Tensor) -> Tensor:
+        out = self.features(x)
+        return out
